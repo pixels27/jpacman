d9345c4f3c5a6d778ed26693cad220fab9f42da4
PRANUNGFUN P <51602295+pixels27@users.noreply.github.com>
2022-11-20 17:54:54 +0700

    testParseMapWrong

--- src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
+++ src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
@@ -1,7 +1,9 @@
 package nl.tudelft.jpacman.level;
 
+import nl.tudelft.jpacman.PacmanConfigurationException;
 import nl.tudelft.jpacman.board.BoardFactory;
 import nl.tudelft.jpacman.npc.ghost.Blinky;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -45,5 +47,80 @@ public class MapParserTest {
         mapParser.parseMap(map);
         Mockito.verify(boardFactory, Mockito.times(1)).createBoard(Mockito.any());
         Mockito.verify(levelFactory, Mockito.times(1)).createGhost();
+    }
+
+    /**
+     * Test for the parseMap method (bad map).
+     * checkMapFormat
+     * Check the correctness of the map lines in the text.
+     * param text Map to be checked
+     * throws PacmanConfigurationException if map is not OK.
+     */
+
+    @Test
+    public void testParseMapWrong1() {
+        PacmanConfigurationException thrown =
+            Assertions.assertThrows(PacmanConfigurationException.class, () -> {
+                MockitoAnnotations.initMocks(this);
+                assertNotNull(boardFactory);
+                assertNotNull(levelFactory);
+                MapParser mapParser = new MapParser(levelFactory, boardFactory);
+                ArrayList<String> map = new ArrayList<>();
+                /*
+                Create a map with inconsistent size between
+                each row or contain invalid characters
+                */
+                map.add("############");
+                map.add("#p        G#");
+                map.add("############");
+                mapParser.parseMap(map);
+            });
+        Assertions.assertEquals("Invalid character at 1,1: p", thrown.getMessage());
+    }
+
+    @Test
+    public void testParseMapWrong2() {
+        PacmanConfigurationException thrown =
+            Assertions.assertThrows(PacmanConfigurationException.class, () -> {
+                MockitoAnnotations.initMocks(this);
+                assertNotNull(boardFactory);
+                assertNotNull(levelFactory);
+                MapParser mapParser = new MapParser(levelFactory, boardFactory);
+                ArrayList<String> map = new ArrayList<>();
+                mapParser.parseMap(map);
+            });
+        Assertions.assertEquals("Input text must consist of at least 1 row.", thrown.getMessage());
+    }
+
+    @Test
+    public void testParseMapWrong3() {
+        PacmanConfigurationException thrown =
+            Assertions.assertThrows(PacmanConfigurationException.class, () -> {
+                MockitoAnnotations.initMocks(this);
+                assertNotNull(boardFactory);
+                assertNotNull(levelFactory);
+                MapParser mapParser = new MapParser(levelFactory, boardFactory);
+                ArrayList<String> map = new ArrayList<>();
+                map.add("");
+                mapParser.parseMap(map);
+            });
+        Assertions.assertEquals("Input text lines cannot be empty.", thrown.getMessage());
+    }
+
+    @Test
+    public void testParseMapWrong4() {
+        PacmanConfigurationException thrown =
+            Assertions.assertThrows(PacmanConfigurationException.class, () -> {
+                MockitoAnnotations.initMocks(this);
+                assertNotNull(boardFactory);
+                assertNotNull(levelFactory);
+                MapParser mapParser = new MapParser(levelFactory, boardFactory);
+                ArrayList<String> map = new ArrayList<>();
+                map.add("#########");
+                map.add("#P        G#");
+                map.add("###############");
+                mapParser.parseMap(map);
+            });
+        Assertions.assertEquals("Input text lines are not of equal width.", thrown.getMessage());
     }
 }
